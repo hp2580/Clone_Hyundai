717fe7bf1ccb7fca0b0dd853c45e7e7af4248260
hp2580 <happycandy0909@gmail.com>
2022-11-04 20:49:38 +0900

    sec1 end / resize evnet added

--- asserts/js/event.js
+++ asserts/js/event.js
@@ -2,6 +2,12 @@ window.onresize = () => {
   if (window.innerWidth < 768) {
   } else {
     header.classList.remove("active");
+    clearActive(menus);
     btnWrap.children[0].classList.remove("active");
+    btnMenu.children[0].style.top = "0";
+    btnMenu.children[0].style.transform = "unset";
+    btnMenu.children[1].style.opacity = "1";
+    btnMenu.children[2].style.top = "auto";
+    btnMenu.children[2].style.transform = "unset";
   }
 };
--- asserts/js/main.js
+++ asserts/js/main.js
@@ -1,6 +1,7 @@
 let header = document.querySelector("header");
 let btnWrap = document.querySelector(".btn_menu");
 let menus = document.querySelectorAll(".menu");
+let btnMenu = btnWrap.children[0];
 
 header.addEventListener("mouseenter", () => {
   if (window.innerWidth > 768) {
@@ -14,22 +15,21 @@ header.addEventListener("mouseleave", () => {
 });
 
 btnWrap.addEventListener("click", (e) => {
-  let btnMenu = btnWrap.children[0];
   if (!btnMenu.classList.contains("active")) {
     btnMenu.classList.add("active");
     header.classList.add("active");
-    btnMenu.children[1].style.opacity = "0";
     btnMenu.children[0].style.top = "50%";
-    btnMenu.children[2].style.top = "50%";
     btnMenu.children[0].style.transform = "rotate(-45deg)";
+    btnMenu.children[1].style.opacity = "0";
+    btnMenu.children[2].style.top = "50%";
     btnMenu.children[2].style.transform = "rotate(45deg)";
   } else {
     btnMenu.classList.remove("active");
     header.classList.remove("active");
-    btnMenu.children[1].style.opacity = "1";
     btnMenu.children[0].style.top = "0";
-    btnMenu.children[2].style.top = "auto";
     btnMenu.children[0].style.transform = "unset";
+    btnMenu.children[1].style.opacity = "1";
+    btnMenu.children[2].style.top = "auto";
     btnMenu.children[2].style.transform = "unset";
   }
 });
@@ -41,8 +41,20 @@ for (let menu of menus) {
       menu.classList.remove("active");
       menu.nextElementSibling.classList.remove("active");
     } else {
+      clearActive(menus);
       menu.classList.add("active");
       menu.nextElementSibling.classList.add("active");
     }
   });
+}
+
+/**
+ *
+ * @param {*} elements Active class를 clear시킬 elements
+ */
+function clearActive(elements) {
+  for (let element of elements) {
+    element.classList.remove("active");
+    element.nextElementSibling.classList.remove("active");
+  }
 }
